3eab712288dfa6a13353999cfe8501aa4d7a4987
PRONOOB895 <71800208+PRONOOB895@users.noreply.github.com>
2020-09-25 01:41:37 -0500

    int numberOfIndexes to maxIndex

--- src/com/pronoob/basicos/ejemplos/BinarioADecimal.java
+++ src/com/pronoob/basicos/ejemplos/BinarioADecimal.java
@@ -2,13 +2,13 @@ package com.pronoob.basicos.ejemplos;
 
 public class BinarioADecimal {
     public static int binaryToDecimal(String binaryNumber) {
-        int numberOfIndexes = binaryNumber.length() - 1;
+        int maxIndex = binaryNumber.length() - 1;
         int answer = 0;
 
         for (int i = 0; i < binaryNumber.length(); i++) {
             int binaryAtIndex = Integer.parseInt(String.valueOf(binaryNumber.charAt(i)));
 
-            answer += Math.pow(2,numberOfIndexes - i) * binaryAtIndex;
+            answer += Math.pow(2,maxIndex - i) * binaryAtIndex;
         }
 
         return answer;
