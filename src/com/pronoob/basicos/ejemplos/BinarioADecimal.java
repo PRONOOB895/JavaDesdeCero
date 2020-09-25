0bfa0c02afb292763ede583ea28d72da98fa821e
PRONOOB895 <71800208+PRONOOB895@users.noreply.github.com>
2020-09-25 02:23:49 -0500

    Añadida clase Scanner para entrada de datos

--- src/com/pronoob/basicos/ejemplos/BinarioADecimal.java
+++ src/com/pronoob/basicos/ejemplos/BinarioADecimal.java
@@ -1,6 +1,10 @@
 package com.pronoob.basicos.ejemplos;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class BinarioADecimal {
+    // Si el String ingresado no es binario, la funcion no dara resultados correctos
     public static int binaryToDecimal(String binaryNumber) {
         int maxIndex = binaryNumber.length() - 1;
         int answer = 0;
@@ -15,7 +19,10 @@ public class BinarioADecimal {
     }
 
     public static void main(String[] args) {
-        String binaryNum = "111";
+        final Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Escribe un numero binario: ");
+        String binaryNum = sc.nextLine();
         System.out.println(binaryToDecimal(binaryNum));
     }
 }
